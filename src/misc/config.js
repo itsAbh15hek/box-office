21308996842059c0dac7074d9be9988d7e884de0
Abh15hek <itsabh15hek@outlook.com>
2021-09-06 03:58:38 +0530

    asd3

--- src/misc/config.js
+++ src/misc/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = `https:api.tvmaze.com`;
+const BASE_URL = `https://api.tvmaze.com`;
 export async function getAPI(queryString) {
   const res = await fetch(`${BASE_URL}${queryString}`).then(r => r.json());
   return res;
